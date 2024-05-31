3db72a39961bb3cbe55281826de869c705ef6475
Gusev Ivan <vanek393956@gmail.com>
2024-05-31 17:16:46 +0200

    dksal

---
+++ laba5/code/index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum_scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div id="form">
+        <form action="save.php" method="post">
+            <label for="email">Email</label>
+            <input type="email" name="email" required>
+
+            <label for="category">Category</label>
+            <select name="category" required>
+            	<?php
+		$list = ['cars', 'food', 'drink', 'other'];
+		for ($i = 0; $i < count($list); $i++)
+		{?>
+		    <option value=<?php echo $list[$i];?>><?php echo $list[$i];?></option>
+		<?php }?>  
+            </select>
+        
+            <label for="title">Title</label>
+            <input type="text" name="title" required>
+
+            <label for="description">Description</label>
+            <textarea rows="3" name="description"></textarea>
+        
+            <input type="submit" value="Save">
+        </form>
+    </div>
+    <div id="table">
+        <table>
+            <thead>
+		<th>Email</th>
+            	<th>Category</th>
+            	<th>Title</th>
+            	<th>Description</th>
+            </thead>
+            <tbody>
+                <?php
+		    $mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+
+		    if (mysqli_connect_errno()) {
+			printf('Подключение к серверу MySQL невозможно. Код ошибки: %s\n', mysqli_connect_error());
+			exit;
+		    }
+		    if ($result = $myslqi->query('SELECT * FROM ad ORDER BY created DESC')) {
+			while ($row = $result->fetch_assoc()){ ?>
+		            <tr>
+			        <td><?php echo $row['email'];?></td>
+				<td><?php echo $row['category'];?></td>
+				<td><?php echo $row['title'];?></td>
+				<td><?php echo $row['description'];?></td>
+		            </tr>
+		        <?php }
+		    }
+	 	?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
---
+++ laba5/code/save.php
@@ -0,0 +1,30 @@
+<?php
+include 'vendor/autoload.php';
+
+function redirectToHome(): void 
+{
+    header('Location: /');
+
+    exit();
+}
+
+if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])) {
+    redirectToHome();
+}
+
+$email = $_POST['email'];
+$category = $_POST['category'];
+$title = $_POST['title'];
+$desc = $_POST['description'];
+
+$mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+
+if (mysqli_connect_errno()) {
+    printf('Подключение к серверу MySQL невозможно. Код ошибки: %s\n', mysqli_connect_error());
+    exit;
+} else {
+    $mysqli->query("INSERT INTO ad (email, title, description, category) VALUES($email, $titile, $desc, $category)");
+}
+
+redirectToHome();
+
